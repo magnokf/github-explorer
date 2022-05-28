ab30a9824245bd7c0901f351a0cbcfdcf14cc352
magnokf <mkfelipe.rj@gmail.com>
2022-05-28 14:59:46 -0300

    useState first look

---
+++ src/components/Counter.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export function Counter(){
+  
+const [counter, setCounter] = useState(0);
+  function onIncrement(){
+    setCounter(counter+1);
+  }
+  function onDecrement(){
+    setCounter(counter-1);
+  }
+  return(
+    <div>
+      <h1>{counter}</h1>
+      <h2>Contador</h2>
+      
+      <button onClick={onIncrement}>Incrementar</button>
+      <button onClick={onDecrement}>Decrementar</button>
+    </div>
+  )
+}
