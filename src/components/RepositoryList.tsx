d45c828842fac3f0147efce75f4b0c5023e8e483
magnokf <mkfelipe.rj@gmail.com>
2022-05-28 17:33:31 -0300

    Typescript  components

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,34 +1,33 @@
 import { useState, useEffect } from 'react'
-import { RepositoryItem } from "./RepositoryItem";
-import '../styles/repository.scss';
+import { RepositoryItem } from './RepositoryItem'
+import '../styles/repository.scss'
 
 //https://api.github.com/orgs/Rocketseat/repos
 
+interface Repository {
+  id: number
+  name: string
+  description: string
+  html_url: string
+}
 
-
-export function RepositoryList(){
-
-  const [repositories, setRepositories] = useState([]);
+export function RepositoryList() {
+  const [repositories, setRepositories] = useState<Repository[]>([])
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/Rocketseat/repos')
-    .then(response => response.json())
-    .then(data => setRepositories(data))
-  }, []);
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, [])
 
-  return(
+  return (
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
       <ul>
         {repositories.map(repository => {
           return <RepositoryItem key={repository.id} repository={repository} />
-          } )}
-        
-        
-
-        
+        })}
       </ul>
     </section>
-    
   )
 }
